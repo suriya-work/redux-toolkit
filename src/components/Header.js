b825c720977574d1224f0d0a71b4576ff1664a30
suriya babaei <suriyalifework60@gmail.com>
2022-12-23 23:13:39 -0800

    create img

--- src/components/Header.js
+++ src/components/Header.js
@@ -50,29 +50,11 @@ const Header = (props) => {
                         <MenuIcon />
                     </IconButton>
                     <img src={Logo} alt="LogoMenu" className={styles.img} />
-                    <Box sx={{ margin: 'auto', display: { xs: 'none', sm: 'block' } }}>
-                        {navItems.map((item) => (
-                            <>
-                                <Button key={item} id={item} sx={{ color: '#fff', fontSize: "12px", margin: "8px", textTransform: 'none' }}>
-                                    {item}
-                                </Button>
-
-                            </>
-                        ))}
-                        <Button sx={{ textTransform: 'none', color: '#fff', borderRadius: '10px', fontSize: '12px', padding: '4px 18px', marginLeft: '10rem' }} variant="outlined">Log in</Button>
-                        <Button sx={{ textTransform: 'none', color: '#fff', borderRadius: '10px', fontSize: '12px', padding: '4px 18px', marginLeft: '10px' }}>Sign Up</Button>
-                        <Button sx={{ textTransform: 'none', color: '#fff', borderRadius: '10px', fontSize: '12px', padding: '4px 18px', marginLeft: '10px' }}>Sign Up</Button>
-                        <Button sx={{ textTransform: 'none', color: '#fff', borderRadius: '10px', fontSize: '12px', padding: '4px 18px', marginLeft: '10px' }}>Sign Up</Button>
-                        <Button sx={{ textTransform: 'none', color: '#fff', borderRadius: '10px', fontSize: '12px', padding: '4px 18px', marginLeft: '10px' }}>Sign Up</Button>
-                        <Button sx={{ textTransform: 'none', color: '#fff', borderRadius: '10px', fontSize: '12px', padding: '4px 18px', marginLeft: '10px' }}>Sign Up</Button>
-                        <Button sx={{ textTransform: 'none', color: '#fff', borderRadius: '10px', fontSize: '12px', padding: '4px 18px', marginLeft: '10px' }}>Sign Up</Button>
-                        <Button sx={{ textTransform: 'none', color: '#fff', borderRadius: '10px', fontSize: '12px', padding: '4px 18px', marginLeft: '10px' }}>Sign Up</Button>
-                        <Button sx={{ textTransform: 'none', color: '#fff', borderRadius: '10px', fontSize: '12px', padding: '4px 18px', marginLeft: '10px' }}>Sign Up</Button>
-                        <Button sx={{ textTransform: 'none', color: '#fff', borderRadius: '10px', fontSize: '12px', padding: '4px 18px', marginLeft: '10px' }}>Sign Up</Button>
-                        <Button sx={{ textTransform: 'none', color: '#fff', borderRadius: '10px', fontSize: '12px', padding: '4px 18px', marginLeft: '10px' }}>Sign Up</Button>
-                        <Button sx={{ textTransform: 'none', color: '#fff', borderRadius: '10px', fontSize: '12px', padding: '4px 18px', marginLeft: '10px' }}>Sign Up</Button>
-                        <Button sx={{ textTransform: 'none', color: '#fff', borderRadius: '10px', fontSize: '12px', padding: '4px 18px', marginLeft: '10px' }}>Sign Up</Button>
-                    </Box>
+                    <img src={Logo} alt="LogoMenu" className={styles.img} />
+                    <img src={Logo} alt="LogoMenu" className={styles.img} />
+                    <img src={Logo} alt="LogoMenu" className={styles.img} />
+                    <img src={Logo} alt="LogoMenu" className={styles.img} />
+                   
                 </Toolbar>
 
             </AppBar>
