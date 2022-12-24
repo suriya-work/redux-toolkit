173dbc37285da577425f455eca351baf21f57dfb
suriya babaei <suriyalifework60@gmail.com>
2022-12-23 23:35:42 -0800

    div

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,24 +3,15 @@ import React from 'react';
 const Header = () => {
     return (
         <div>
-            <h1>hello</h1>
-            <h1>Sala,</h1>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem libero minima </p>
             <div>
-<<<<<<< HEAD
-                <h1></h1>
-                <h1></h1>
-                <h1></h1>
-                <h1></h1>
-=======
-                <img src="" alt="" />
-                <img src="" alt="" />
-                <img src="" alt="" />
-                <img src="" alt="" />
->>>>>>> music
+                <div>
+                    <div>
+                        
+                    </div>
+                </div>
             </div>
+            
         </div>
-        
     );
 };
 
