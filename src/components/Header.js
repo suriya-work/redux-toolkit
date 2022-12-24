82fc091eb8de8cedd0d69b1f4551434cd74daee9
suriya babaei <suriyalifework60@gmail.com>
2022-12-23 23:29:36 -0800

    img

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,12 @@ const Header = () => {
             <h1>hello</h1>
             <h1>Sala,</h1>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem libero minima </p>
+            <div>
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <img src="" alt="" />
+            </div>
         </div>
     );
 };
