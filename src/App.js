879a03a73cf4c1e05494642e46048148ef62fa73
suriya babaei <suriyalifework60@gmail.com>
2022-12-25 00:31:43 -0800

    search

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { allRoute } from './routes/Routers';
+import Search from './page/Search';
 const App = () => {
   return (
     <div>
+      <Search />
       <Routes>
         {allRoute.map(({ path, component: Component }, index) => (
           <Route
