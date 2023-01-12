7a40119601cb5eee4aa2bc50f8525db433673e2b
suriya babaei <suriyalifework60@gmail.com>
2023-01-12 02:25:17 -0800

    create notfound

---
+++ src/components/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { notFound } from '../styles/NotFoundStyle';
+
+const NotFound = () => {
+    return (
+        <notFound>
+            <h2>404</h2>
+            <p>نتیجه ای یافت نشد</p>
+        </notFound>
+    );
+};
+
+export default NotFound;
---
+++ src/styles/NotFoundStyle.js
@@ -0,0 +1,14 @@
+import { styled } from "@mui/material/styles";
+import { Typography, Box, rgbToHex } from "@mui/material";
+
+
+export const notFound = styled(Box)({
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    textAlign:'center',
+    alignItems: 'center',
+    color: '#686868',
+    minHeight: '88vh',
+
+})
