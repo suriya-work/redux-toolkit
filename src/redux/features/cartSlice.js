0acc6a08d2e986cdf40917bb155844a6ea4c47a4
suriya babaei <suriyalifework60@gmail.com>
2023-01-20 22:44:10 -0800

    create cartappsloce

--- src/redux/features/cartSlice.js
+++ src/redux/features/cartSlice.js
@@ -2,7 +2,9 @@ import { createSlice } from "@reduxjs/toolkit";
 
 
 const initialState = {
-    cartItems: [],
+    cartItems: localStorage.getItem("cartItems")
+        ? JSON.parse(localStorage.getItem("cartItems"))
+        : [],
     cartTotalQuontity: 0,
     cartTotalAmount: 0,
 }
@@ -11,7 +13,6 @@ const initialState = {
 const cartSlice = createSlice({
     name: "cart",
     initialState,
-
     reducers: {
         addToCart(state, action) {
             state.cartItems.push(action.payload);
