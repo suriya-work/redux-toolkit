b698c09b5d26a45911a2058c5acc117d88eb5fa3
suriya babaei <suriyalifework60@gmail.com>
2023-01-23 04:43:29 -0800

    store midlle

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
-import cartSlice from './features/cartSlice';
+import cartSlice, { cartsFetch } from './features/cartSlice';
+import { cartSliceApi } from './features/cartSliceApi';
 const store = configureStore({
     reducer: {
         cart: cartSlice,
+        [cartSliceApi.reducerPath]: cartSliceApi.reducer,
     },
-})
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(cartSliceApi.middleware),
+});
+
+store.dispatch(cartsFetch());
 
 
 export default store;
