21d7eda057760cb1715ce031b85304d90c980250
suriya babaei <suriyalifework60@gmail.com>
2023-01-23 04:42:21 -0800

    create home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React , { useEffect } from 'react';
+import { useDispatch } from "react-redux";
+//API
+import { cartSliceApi , useGetAllCartQuery } from '../redux/features/cartSliceApi';
 
 const Home = () => {
+    // const { items, status } = useSelector(state => state.carts)
+    const dispatch = useDispatch()
+    const { data, error, isLoading } = useGetAllCartQuery();
+    // useEffect(() => {
+    //     dispatch(useGetAllCartQuery())
+    // }, [])
     return (
         <div>
-            Home
+            {
+                isLoading ? (
+                    <p>در حال بارگذاری...</p>
+                ) : error ? (
+                    <p>خطا...</p>
+                ) : (
+                    <>
+                        <h2>محصولات جدید</h2>
+                        <div>
+                            {
+                                data.map(cart => <div key={cart.id}>
+                                    <h3>{cart.name}</h3>
+                                    <img src={cart.image} alt={cart.name} />
+                                </div>)
+                            }
+
+                        </div>
+                    </>
+                )}
         </div>
     );
 };
